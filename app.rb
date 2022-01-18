a1d10581d9c39f8a0976fe511a999dda0986048a
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-01-18 21:03:29 +0100

    user system nästan klar

--- app.rb
+++ app.rb
@@ -1,16 +1,114 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
+require 'base64'
+require 'bcrypt'
 
-get ('/') do
-    slim(:'lootbox/index')
+
+enable :sessions
+
+get('/') do
+    db=SQLite3::Database.new('db/nft-lootbox.db')
+    db.results_as_hash=true
+    result=db.execute("SELECT * FROM Lootbox")
+    
+    slim(:'lootbox/index', locals:{lootboxes:result})
 end
 
-get ('/lootbox/new') do
+get('/lol')do
+    db=SQLite3::Database.new('db/nft-lootbox.db')
+    db.results_as_hash=true
+    result=db.execute("SELECT * FROM Lootbox WHERE id=10").first
+    imgBlob=result["img1"]
+    # send_file ''
+    # "<img src=#{imgBlob}>"
+    # srcString="data:image/png;base64, #{imgBlob}"
+    # blobString="lol"
+    srcString="'data:image/png;base64,#{imgBlob}'"
+    
+    
+
+    "<img src=#{srcString}>"
+end
+
+get('/login')do
+    slim(:login)
+end
+
+get('/logout')do
+    session
+end
+
+get('/lootbox/new') do
     slim(:'lootbox/new')
 end
 
-post ('/lootbox/create') do
+get('/register')do
+    slim(:register)
+end
+
+post('/login')do
+    username=params[:username]
+    password=params[:password]
+    db=SQLite3::Database.new('db/nft-lootbox.db')
+    db.results_as_hash=true
+    result=db.execute('SELECT * FROM users WHERE username = ?', username).first
+    begin
+        pwdigest=result["pwdigest"]
+        id=result["id"]
+    
+        if BCrypt::Password.new(pwdigest)==password
+            session[:loginError]=false
+            session[:id]=id
+            session[:auth]=true
+            session[:firstLetter]=username.split('')[0]
+            redirect('/')
+            
+        else
+            session[:loginError]=true
+            redirect('/login')
+        end
+        
+    rescue => exception
+        p exception
+        session[:loginError]=true
+        redirect('/login')
+
+    end
+
+end
+
+post('/register')do
+    username=params[:username]
+    password=params[:password]
+    password_confirm=params[:password_confirm]
+       
+    if password==password_confirm || username=="" || password==""
+        begin
+            password_digest=BCrypt::Password.create(password)
+            db=SQLite3::Database.new('db/nft-lootbox.db')
+            db.execute('INSERT INTO users (username, pwdigest) VALUES(?, ?)', username, password_digest )
+            id=db.execute("SELECT id FROM users WHERE username=?", username).first
+            session[:error]=false
+            session[:auth]=true
+            session[:id]=id
+            session[:firstLetter]=username.split('')[0]
+            redirect('/')
+            
+        rescue => exception
+            p exception
+            session[:registerError]=true
+            redirect('/register')
+        end
+    else
+        p 'dont match'
+        session[:registerError]=true
+        redirect('/register')
+    end
+    
+end
+
+post('/lootbox/create') do
     title=params[:title]
     rarity=params[:rarity]
     price=params[:price]
@@ -30,9 +128,16 @@ post ('/lootbox/create') do
    
     
     if images.length==1
-       read1=images[0].read
-       blob1=SQLite3::Blob.new read1
-       db.execute("INSERT INTO Lootbox (img1, rarity, title, price) VALUES(?, ?, ?, ?)", blob1, rarity, title, price)
+       
+
+        # png=File.open "public/img.png", 'rb'
+
+        enc1= Base64.encode64(images[0].read)  
+
+    #    read1=images[0].read
+    #    blob1=SQLite3::Blob.new read1
+    #    p blob1
+       db.execute("INSERT INTO Lootbox (img1, rarity, title, price) VALUES(?, ?, ?, ?)", enc1, rarity, title, price)
     elsif images.length==2
         read1=images[0].read
         blob1=SQLite3::Blob.new read1
